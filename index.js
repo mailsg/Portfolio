aaefd1f1feab591e3ed533bf0487d064c0168da3
noelfoka <nomgnenoel@gmail.com>
2023-04-21 09:03:34 +0100

    Add JS Function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mobileFrame = document.querySelector('[main_page]');
+const mobileFrame = document.querySelector("[main_page]");
 
 // Pop Up window for projects
-const workHeaderSection = document.querySelector('#work_link');
+const workHeaderSection = document.querySelector("#work_link");
 
 const workHeaderSubSection = () => {
-  const workHeaderSectionTitle = document.createElement('div');
-  workHeaderSectionTitle.className = 'works-title-block';
+  const workHeaderSectionTitle = document.createElement("div");
+  workHeaderSectionTitle.className = "works-title-block";
   workHeaderSectionTitle.innerHTML = `
     <h3 class="title flex">My Recent Works</h3> 
       <div class="divider_1"> 
@@ -17,8 +17,8 @@ const workHeaderSubSection = () => {
     </div>`;
   workHeaderSection.appendChild(workHeaderSectionTitle);
 
-  const yogaImageCard = document.createElement('div');
-  yogaImageCard.className = 'works-card';
+  const yogaImageCard = document.createElement("div");
+  yogaImageCard.className = "works-card";
   yogaImageCard.innerHTML = `
     <div class="yoga-placeholder">
       <img src="images/yoga-image.png" alt="yoga">
@@ -38,11 +38,11 @@ const workHeaderSubSection = () => {
     </div>`;
   workHeaderSection.appendChild(yogaImageCard);
   // Add event listener to the button
-  const btnTypeAMb = yogaImageCard.querySelector('.btn-type-a');
-  btnTypeAMb.addEventListener('click', () => {
+  const btnTypeAMb = yogaImageCard.querySelector(".btn-type-a");
+  btnTypeAMb.addEventListener("click", () => {
     // Create the popup modal
-    const popupMb = document.createElement('section');
-    popupMb.id = 'details-popup';
+    const popupMb = document.createElement("section");
+    popupMb.id = "details-popup";
     popupMb.innerHTML = `
       <div class="popup-header flex">
       <h3 class="bold-text">Multi-Post Stories</h3>
@@ -67,9 +67,9 @@ const workHeaderSubSection = () => {
       </div>`;
     mobileFrame.appendChild(popupMb);
 
-    const popupDeleteYogaMb = document.querySelector('#popup-close');
-    const popupBodyYogaMb = document.querySelector('.popup-header');
-    popupDeleteYogaMb.addEventListener('click', () => {
+    const popupDeleteYogaMb = document.querySelector("#popup-close");
+    const popupBodyYogaMb = document.querySelector(".popup-header");
+    popupDeleteYogaMb.addEventListener("click", () => {
       popupBodyYogaMb.parentNode.remove();
     });
   });
@@ -77,8 +77,8 @@ const workHeaderSubSection = () => {
 workHeaderSubSection();
 
 function worksDesktop() {
-  const worksCardDesktop = document.createElement('div');
-  worksCardDesktop.className = 'works-card-desktop';
+  const worksCardDesktop = document.createElement("div");
+  worksCardDesktop.className = "works-card-desktop";
   worksCardDesktop.innerHTML = `
   <img src="images/yoga-image.png" class="project-cards flex">
   <div class="extras side-text">
@@ -97,11 +97,11 @@ function worksDesktop() {
     <button type="button" class="btn-type-a">See project</button>
   </div>`;
   // Add event listener to the button
-  const btnTypeA = worksCardDesktop.querySelector('.btn-type-a');
-  btnTypeA.addEventListener('click', () => {    
+  const btnTypeA = worksCardDesktop.querySelector(".btn-type-a");
+  btnTypeA.addEventListener("click", () => {
     // Create the popup modal
-    const popup = document.createElement('section');
-    popup.id = 'details-popup';
+    const popup = document.createElement("section");
+    popup.id = "details-popup";
     popup.innerHTML = `
       <div class="popup-header flex">
       <h3 class="bold-text">Multi-Post Stories</h3>
@@ -127,9 +127,9 @@ function worksDesktop() {
       </div>`;
     mobileFrame.appendChild(popup);
 
-    const popupDeleteYoga = document.querySelector('#popup-close');
-    const popupBodyYoga = document.querySelector('.popup-header');
-    popupDeleteYoga.addEventListener('click', () => {      
+    const popupDeleteYoga = document.querySelector("#popup-close");
+    const popupBodyYoga = document.querySelector(".popup-header");
+    popupDeleteYoga.addEventListener("click", () => {
       popupBodyYoga.parentNode.remove();
     });
   });
@@ -138,151 +138,157 @@ function worksDesktop() {
 }
 worksDesktop();
 
-const cardsContainer = document.createElement('div');
-cardsContainer.className = 'grid-container';
+const cardsContainer = document.createElement("div");
+cardsContainer.className = "grid-container";
 
 const projectArray = [
   {
-    name: 'Professional Art<br>Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp',
-    'Featured image': {
+    name: "Professional Art<br>Printing Data",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp",
+    "Featured image": {
       mobile: {
-        src: 'images/mob_img_1.png',
-        alt: '',
+        src: "images/mob_img_1.png",
+        alt: "",
       },
       desktop: {
-        src: 'images/desktop_img_Placeholder_1.png',
-        alt: '',
+        src: "images/desktop_img_Placeholder_1.png",
+        alt: "",
       },
     },
     technologies: {
-      tech1: 'CSS',
-      tech2: 'HTML',
-      tech3: 'Javascript',
+      tech1: "CSS",
+      tech2: "HTML",
+      tech3: "Javascript",
     },
-    'Link to live version': '#',
-    'Link to source': '#',
+    "Link to live version": "#",
+    "Link to source": "#",
   },
 
   {
-    name: 'Data Dashboard<br>Healthcare',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp',
-    'Featured image': {
+    name: "Data Dashboard<br>Healthcare",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp",
+    "Featured image": {
       mobile: {
-        src: 'images/mob_img_1.png',
-        alt: '',
+        src: "images/mob_img_1.png",
+        alt: "",
       },
       desktop: {
-        src: 'images/desktop_img_Placeholder_2.png',
-        alt: '',
+        src: "images/desktop_img_Placeholder_2.png",
+        alt: "",
       },
     },
     technologies: {
-      tech1: 'CSS',
-      tech2: 'HTML',
-      tech3: 'Javascript',
+      tech1: "CSS",
+      tech2: "HTML",
+      tech3: "Javascript",
     },
-    'Link to live version': '#',
-    'Link to source': '#',
+    "Link to live version": "#",
+    "Link to source": "#",
   },
 
   {
-    name: '<br>Website Portfolio',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp',
-    'Featured image': {
+    name: "<br>Website Portfolio",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp",
+    "Featured image": {
       mobile: {
-        src: 'images/mob_img_1.png',
-        alt: '',
+        src: "images/mob_img_1.png",
+        alt: "",
       },
       desktop: {
-        src: 'images/desktop_img_Placeholder_3.png',
-        alt: '',
+        src: "images/desktop_img_Placeholder_3.png",
+        alt: "",
       },
     },
     technologies: {
-      tech1: 'CSS',
-      tech2: 'HTML',
-      tech3: 'Javascript',
+      tech1: "CSS",
+      tech2: "HTML",
+      tech3: "Javascript",
     },
-    'Link to live version': '#',
-    'Link to source': '#',
+    "Link to live version": "#",
+    "Link to source": "#",
   },
 
   {
-    name: 'Professional Art<br>Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp',
-    'Featured image': {
+    name: "Professional Art<br>Printing Data",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp",
+    "Featured image": {
       mobile: {
-        src: 'images/mob_img_1.png',
-        alt: '',
+        src: "images/mob_img_1.png",
+        alt: "",
       },
       desktop: {
-        src: 'images/mob_img_1.png',
-        alt: '',
+        src: "images/mob_img_1.png",
+        alt: "",
       },
     },
     technologies: {
-      tech1: 'CSS',
-      tech2: 'HTML',
-      tech3: 'Javascript',
+      tech1: "CSS",
+      tech2: "HTML",
+      tech3: "Javascript",
     },
-    'Link to live version': '#',
-    'Link to source': '#',
+    "Link to live version": "#",
+    "Link to source": "#",
   },
 
   {
-    name: 'Data Dashboard<br>Healthcare',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp',
-    'Featured image': {
+    name: "Data Dashboard<br>Healthcare",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp",
+    "Featured image": {
       mobile: {
-        src: 'images/mob_img_1.png',
-        alt: '',
+        src: "images/mob_img_1.png",
+        alt: "",
       },
       desktop: {
-        src: 'images/desktop_img_Placeholder_2.png',
-        alt: '',
+        src: "images/desktop_img_Placeholder_2.png",
+        alt: "",
       },
     },
     technologies: {
-      tech1: 'CSS',
-      tech2: 'HTML',
-      tech3: 'Javascript',
+      tech1: "CSS",
+      tech2: "HTML",
+      tech3: "Javascript",
     },
-    'Link to live version': '#',
-    'Link to source': '#',
+    "Link to live version": "#",
+    "Link to source": "#",
   },
 
   {
-    name: '<br>Website Portfolio',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp',
-    'Featured image': {
+    name: "<br>Website Portfolio",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry standard.<br>&nbsp",
+    "Featured image": {
       mobile: {
-        src: 'images/mob_img_1.png',
-        alt: '',
+        src: "images/mob_img_1.png",
+        alt: "",
       },
       desktop: {
-        src: 'images/desktop_img_Placeholder_3.png',
-        alt: '',
+        src: "images/desktop_img_Placeholder_3.png",
+        alt: "",
       },
     },
     technologies: {
-      tech1: 'CSS',
-      tech2: 'HTML',
-      tech3: 'Javascript',
+      tech1: "CSS",
+      tech2: "HTML",
+      tech3: "Javascript",
     },
-    'Link to live version': '#',
-    'Link to source': '#',
+    "Link to live version": "#",
+    "Link to source": "#",
   },
 ];
 
 function prjCards(arr) {
   for (let i = 0; i < arr.length; i += 1) {
-    const card = document.createElement('div');
-    card.className = 'grid-item';
+    const card = document.createElement("div");
+    card.className = "grid-item";
     const contentCard = arr[i];
     card.innerHTML = `
-    <img src=${contentCard['Featured image'].mobile.src} alt=${contentCard['Featured image'].alt} width class="project-cards-mobile">
-    <img class='project-cards-desktop' src=${contentCard['Featured image'].desktop.src} alt=${contentCard['Featured image'].alt}>
+    <img src=${contentCard["Featured image"].mobile.src} alt=${contentCard["Featured image"].alt} width class="project-cards-mobile">
+    <img class='project-cards-desktop' src=${contentCard["Featured image"].desktop.src} alt=${contentCard["Featured image"].alt}>
     <div class="image-text flex">
       <div>
         <h3 class="bold-text flex">${contentCard.name}</h3>
@@ -302,16 +308,16 @@ function prjCards(arr) {
 prjCards(projectArray);
 workHeaderSection.appendChild(cardsContainer);
 
-const cardButton = document.querySelectorAll('.btn-type-b');
+const cardButton = document.querySelectorAll(".btn-type-b");
 function popupButton(arr, elem) {
   for (let i = 0; i < arr.length; i += 1) {
     const popupBtn = arr[i];
-    popupBtn.addEventListener('click', () => {
+    popupBtn.addEventListener("click", () => {
       for (let j = 0; j < elem.length; j += 1) {
         if (j === i) {
           const features = elem[j];
-          const featuresContent = document.createElement('section');
-          featuresContent.id = 'details-popup';
+          const featuresContent = document.createElement("section");
+          featuresContent.id = "details-popup";
           featuresContent.innerHTML = `
             <div class="popup-header flex">
             <h3 class="bold-text">${features.name}</h3>
@@ -326,16 +332,16 @@ function popupButton(arr, elem) {
               <div class="side-text">
                 <div class="works-description popup-text">${features.description}</div>
                 <div class="demoButtons flex">
-                  <a href=${features['Link to live version']} class="btn-type-a">See live <img src="images/export-icon.png" alt="" width='13'></a>
-                  <a href=${features['Link to live version']} class="btn-type-a">See source <img src="images/github-icon.png" alt="" width='13'></a>
+                  <a href=${features["Link to live version"]} class="btn-type-a">See live <img src="images/export-icon.png" alt="" width='13'></a>
+                  <a href=${features["Link to live version"]} class="btn-type-a">See source <img src="images/github-icon.png" alt="" width='13'></a>
                 </div>
               </div>
             </div>`;
           mobileFrame.appendChild(featuresContent);
 
-          const popupDelete = document.querySelector('#popup-close');
-          const popupBody = document.querySelector('.popup-header');
-          popupDelete.addEventListener('click', () => {
+          const popupDelete = document.querySelector("#popup-close");
+          const popupBody = document.querySelector(".popup-header");
+          popupDelete.addEventListener("click", () => {
             popupBody.parentNode.remove();
           });
         }
@@ -346,54 +352,54 @@ function popupButton(arr, elem) {
 popupButton(cardButton, projectArray);
 
 // Adding functionality to mobile menu
-const hamburgerButton = document.querySelector('[hb_menu_list]');
+const hamburgerButton = document.querySelector("[hb_menu_list]");
 
 const showMobileMenu = () => {
-  const modal = document.createElement('section');
-  modal.id = 'custom-modal';
+  const modal = document.createElement("section");
+  modal.id = "custom-modal";
 
-  const deleteButton = document.createElement('div');
-  deleteButton.textContent = 'X';
-  deleteButton.id = 'modal-delete';
+  const deleteButton = document.createElement("div");
+  deleteButton.textContent = "X";
+  deleteButton.id = "modal-delete";
   modal.appendChild(deleteButton);
 
-  const menuList = document.createElement('ul');
-  menuList.id = 'menu-list';
+  const menuList = document.createElement("ul");
+  menuList.id = "menu-list";
 
-  const listItem1 = document.createElement('li');
-  const portfolio = document.createElement('a');
-  portfolio.href = '#work';
-  portfolio.className = 'menu-content';
-  portfolio.textContent = 'Portfolio';
+  const listItem1 = document.createElement("li");
+  const portfolio = document.createElement("a");
+  portfolio.href = "#work";
+  portfolio.className = "menu-content";
+  portfolio.textContent = "Portfolio";
   listItem1.appendChild(portfolio);
-  portfolio.addEventListener('click', () => {
-    modal.style.visibility = 'hidden';
-    const workSection = document.querySelector('#work-link');
-    workSection.scrollIntoView({ behaviour: 'smooth' });
+  portfolio.addEventListener("click", () => {
+    modal.style.visibility = "hidden";
+    const workSection = document.querySelector("#work-link");
+    workSection.scrollIntoView({ behaviour: "smooth" });
   });
 
-  const listItem2 = document.createElement('li');
-  const about = document.createElement('a');
-  about.href = '#about';
-  about.className = 'menu-content';
-  about.textContent = 'About';
+  const listItem2 = document.createElement("li");
+  const about = document.createElement("a");
+  about.href = "#about";
+  about.className = "menu-content";
+  about.textContent = "About";
   listItem2.appendChild(about);
-  about.addEventListener('click', () => {
-    modal.style.visibility = 'hidden';
-    const aboutSection = document.querySelector('#about-link');
-    aboutSection.scrollIntoView({ behaviour: 'smooth' });
+  about.addEventListener("click", () => {
+    modal.style.visibility = "hidden";
+    const aboutSection = document.querySelector("#about-link");
+    aboutSection.scrollIntoView({ behaviour: "smooth" });
   });
 
-  const listItem3 = document.createElement('li');
-  const contact = document.createElement('a');
-  contact.href = '#contact_section';
-  contact.className = 'menu-content';
-  contact.textContent = 'Contact';
+  const listItem3 = document.createElement("li");
+  const contact = document.createElement("a");
+  contact.href = "#contact_section";
+  contact.className = "menu-content";
+  contact.textContent = "Contact";
   listItem3.appendChild(contact);
-  contact.addEventListener('click', () => {
-    modal.style.visibility = 'hidden';
-    const contactSection = document.querySelector('#contact-link');
-    contactSection.scrollIntoView({ behaviour: 'smooth' });
+  contact.addEventListener("click", () => {
+    modal.style.visibility = "hidden";
+    const contactSection = document.querySelector("#contact-link");
+    contactSection.scrollIntoView({ behaviour: "smooth" });
   });
 
   menuList.appendChild(listItem1);
@@ -403,15 +409,22 @@ const showMobileMenu = () => {
   mobileFrame.appendChild(modal);
 
   const removeModal = () => {
-    modal.style.visibility = 'hidden';
+    modal.style.visibility = "hidden";
   };
 
-  deleteButton.addEventListener('click', removeModal);
+  deleteButton.addEventListener("click", removeModal);
 };
 
-hamburgerButton.addEventListener('click', showMobileMenu);
+hamburgerButton.addEventListener("click", showMobileMenu);
 
 //Email validation code below...
-const name = document.querySelectorAll('');
-const email = document.querySelectorAll('');
-const form = document.querySelectorAll('');
+const name = document.querySelectorAll("");
+const email = document.querySelectorAll("");
+const form = document.querySelectorAll("");
+
+function isValid(str) {
+  if (str.toLowerCase() !== str) {
+    return false;
+  }
+  return true;
+}
